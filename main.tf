6defd43f78557b28cc2ed309a8816eef31421b46
acider19 <acider19@gmail.com>
2025-09-14 12:54:21 +0300

    Add resources

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
 # first terraform project
+resource "docker_image" "nginx" {
+  name = var.containers.nginx.image
+  keep_locally = true
+}
+
+resource "docker_image" "wordpress" {
+  name = var.containers.wordpress.image
+  keep_locally = true
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,39 @@
 # my vars
 variable "string" {
-    type = string
-    default = "junk"
-  
+  type = string
+  default = "junk"
+}
+
+variable "list_of_string" {
+  type = list(string)
+  default = ["a", "b", "c"]
+}
+
+variable "containers" {
+    type = map(object({
+      name = string
+      image = string
+      ports = object({
+        external = number
+        internal = number
+      })
+    }))
+    default = {
+      nginx = {
+        name = "reverse-proxy-nginx"
+        image = "nginx:1.21.1"
+        ports = {
+          internal = 80
+          external = 1080
+      }
+     },
+     wordpress = {
+        name = "web-wordpress"
+        image = "wordpress:latest"
+        ports = {
+            internal = 80
+            external = 2080
+        }
+     }
+   }
 }
