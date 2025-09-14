85fec6755d77e03df9d336739095cecbd28dde77
acider19 <acider19@gmail.com>
2025-09-14 13:16:03 +0300

    Add resources

--- main.tf
+++ main.tf
@@ -3,8 +3,23 @@ resource "docker_image" "nginx" {
   name = var.containers.nginx.image
   keep_locally = true
 }
-
 resource "docker_image" "wordpress" {
   name = var.containers.wordpress.image
   keep_locally = true
+}
+resource "docker_container" "nginx" {
+  name  = "project-${var.containers.nginx.name}"
+  image = docker_image.nginx.image_id
+  ports {
+    internal = var.containers.nginx.ports.internal
+    external = var.containers.nginx.ports.external
+  }
+}
+resource "docker_container" "wordpress" {
+  name  = "project-${var.containers.wordpress.name}"
+  image = docker_image.wordpress.image_id
+  ports {
+    internal = var.containers.wordpress.ports.internal
+    external = var.containers.wordpress.ports.external
+  }
 }
